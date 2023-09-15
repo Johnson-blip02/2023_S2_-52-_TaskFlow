16f193fb1c3bba8aaff07362608c0e4a247f6a06
teriberry <55357603+tmatalavea@users.noreply.github.com>
2023-09-16 10:41:02 +1200

    style: modified all entries to have no underline throughout app

--- TaskFlow/App.xaml.cs
+++ TaskFlow/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using TaskFlow.Model;
+﻿using Android.Content.Res;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+using TaskFlow.Model;
 
 namespace TaskFlow;
 
@@ -11,8 +13,18 @@ public partial class App : Application
 
         InitializeComponent();
 
+		ModifyHandlers();
+
 		TodoModel = new TodoModel();
 
 		MainPage = new AppShell();
+	}
+
+	private void ModifyHandlers()
+	{
+		Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+		{
+			handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+		});
 	}
 }
