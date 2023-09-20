06f247fd1d8ccf7f7f23487e3dd6456e16473c46
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2023-09-21 02:32:17 +1200

    Further Task Editing
    bugfix: Fixed bugs relating to task editing,
    Tasks now can be properly saved and edited.
    
    feat: Task Info Popup Reusability
    The TodoPopup is reuable and only requires two paramaters to be specified when implementing the xaml. This is the "RunWhenSave" and "Todo" parameter. RunWhenSave is a bindable ICommand which will run when the save button is pressed. Intended to be used for updating the database. The Todo paramater is the Todo object which is to be bound to the popup. Changes made in the popup will be reflected in the bound todo object.
    
    feat: Editing cancel
    Cancelling an edit will cause the popup to show the orignal data.
    
    feat: Task popup importance
    Tasks now have editable and viewable importance buttons.
    
    feat: Task colour picking
    Task popup now has labels for colour. Task's colour can be changed properly.
    
    todo: Task deletion
    Allow for tasks to be deleted

--- TaskFlow/View/TodoPopup.xaml.cs
+++ TaskFlow/View/TodoPopup.xaml.cs
@@ -1,8 +1,7 @@
 using System.Collections.ObjectModel;
 using TaskFlow.Model;
-using Syncfusion.Maui.Popup;
-using System.Security.Cryptography.X509Certificates;
 using System.Windows.Input;
+using Practice.Model;
 
 namespace TaskFlow.View;
 
@@ -11,7 +10,7 @@ public partial class TodoPopup : ContentView
 	public static readonly BindableProperty TodoProperty = BindableProperty.Create(nameof(Todo), typeof(TodoItem), typeof(TodoPopup), null);
 	public static readonly BindableProperty IsOpenProperty = BindableProperty.Create(nameof(IsOpen), typeof(bool), typeof(TodoPopup), false);
 	public static readonly BindableProperty RunWhenSaveProperty = BindableProperty.Create(nameof(RunWhenSave), typeof(ICommand), typeof(ICommand), null);
-
+    
     /// <summary>
     /// The bound command which is to be run when the popup's save
     /// button is pressed.
@@ -32,13 +31,96 @@ public partial class TodoPopup : ContentView
     }
 
     /// <summary>
+    /// Value to store the pre-edited importance value
+    /// </summary>
+    private int OriginalImportance;
+
+    /// <summary>
     /// The bound todo object
     /// </summary>
 	public TodoItem Todo
 	{
-		get => (TodoItem)GetValue(TodoProperty);
-		set => SetValue(TodoProperty, value);
-	}
+        get
+        {
+            if ((TodoItem)GetValue(TodoProperty) != null)
+            {
+                TempTodo = (TodoItem)GetValue(TodoProperty);
+
+                _colorLabel = DefinedColorsExtension.Parse(Color.Parse(((TodoItem)GetValue(TodoProperty)).Color));
+                _tempColorLabel = _colorLabel;
+
+                OriginalImportance = ((TodoItem)GetValue(TodoProperty)).Importance;
+                ResetSelected(((TodoItem)GetValue(TodoProperty)).Importance);
+
+                OnPropertyChanged(nameof(ColourLabel));
+                OnPropertyChanged(nameof(TempColourLabel));
+                OnPropertyChanged(nameof(TempTodo));
+            }
+
+            return (TodoItem)GetValue(TodoProperty);
+        }
+        set
+        {
+            SetValue(TodoProperty, value);
+            OnPropertyChanged(nameof(Todo));
+        }
+    }
+
+    private TodoItem _tempTodo;
+    /// <summary>
+    /// Editable Todo object
+    /// </summary>
+    public TodoItem TempTodo 
+    { 
+        get
+        {
+            return _tempTodo;
+        }
+        set
+        {
+            _tempTodo = value;
+            OnPropertyChanged(nameof(TempTodo));
+        }
+    }
+
+    private string _tempColorLabel;
+    /// <summary>
+    /// Editable label for the current colour
+    /// </summary>
+    public string TempColourLabel
+    {
+        get => _tempColorLabel;
+        set
+        {
+            if (TempTodo != null)
+            {
+                _tempColorLabel = value;
+                TempTodo.Color = DefinedColorsExtension.Parse(value).ToArgbHex();
+                OnPropertyChanged(nameof(TempTodo));
+            }
+            OnPropertyChanged(nameof(TempColourLabel));
+        }
+    }
+
+    private string _colorLabel;
+    /// <summary>
+    /// Non editable label for the current colour
+    /// </summary>
+    public string ColourLabel
+    {
+        get => _colorLabel;
+        set
+        {
+            if((TodoItem)GetValue(TodoProperty) != null)
+            {
+                _colorLabel = value;
+                Todo.Color = DefinedColorsExtension.Parse(value).ToArgbHex();
+                OnPropertyChanged(nameof(Todo));
+            }
+
+            OnPropertyChanged(nameof(ColourLabel));
+        }
+    }
 
     /// <summary>
     /// The list of selectable time blocks
@@ -56,11 +138,44 @@ public partial class TodoPopup : ContentView
 	{
 		get => DateTime.Today;
 	}
+    /// <summary>
+    /// List of all selectable colours
+    /// </summary>
+    public List<string> ColoursList { get; set; }
+
+    private TimeSpan _tempSelectedTime;
+    /// <summary>
+    /// Editable time selection data
+    /// </summary>
+    public TimeSpan TempSelectedTime
+    {
+        get => _tempSelectedTime;
+        set
+        {
+            _tempSelectedTime = value;
+            TempTodo.DueDate = TempTodo.DueDate.Date;
+            TempTodo.DueDate = TempTodo.DueDate.AddMinutes(value.TotalMinutes);
+        }
+    }
+
+    //The following boolean are for enabling the state of which
+    //importance button is highlighted/selected
+    public bool LowEnable { get; set; }
+    public bool MedEnable { get; set; }
+    public bool HighEnable { get; set; }
+    public bool VHighEnable { get; set; }
 
     public TodoPopup()
 	{
+        var colours = new List<string>();
+        foreach (string dc in Enum.GetNames<DefinedColors>())
+        {
+            colours.Add(dc);
+        }
+        ColoursList = colours;
         InitializeComponent();
     }
+
 
     /// <summary>
     /// Opens the editable popup, closes the non-editable one
@@ -81,7 +196,12 @@ public partial class TodoPopup : ContentView
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
         todoPopup.IsOpen = false;
-		RunWhenSave.Execute(Todo);
+
+        ColourLabel = TempColourLabel;
+
+        RunWhenSave.Execute(Todo);
+        OnPropertyChanged(nameof(Todo));
+
         todoPopupHidden.IsOpen = true;
     }
 
@@ -92,5 +212,73 @@ public partial class TodoPopup : ContentView
     {
         todoPopup.IsOpen = false;
         todoPopupHidden.IsOpen = true;
+        ResetSelected(OriginalImportance);
+    }
+
+    /// <summary>
+    /// Low importance button event handler
+    /// </summary>
+    private void Low_Pressed(object sender, EventArgs e)
+    {
+        TempTodo.Importance = 1;
+        ResetSelected(1);
+    }
+    /// <summary>
+    /// Medium importance button event handler
+    /// </summary>
+    private void Med_Pressed(object sender, EventArgs e)
+    {
+        TempTodo.Importance = 2;
+        ResetSelected(2);
+    }
+    /// <summary>
+    /// High importance button event handler
+    /// </summary>
+    private void High_Pressed(object sender, EventArgs e)
+    {
+        TempTodo.Importance = 3;
+        ResetSelected(3);
+    }
+    /// <summary>
+    /// Urgent importance button event handler
+    /// </summary>
+    private void Urgent_Pressed(object sender, EventArgs e)
+    {
+        TempTodo.Importance = 4;
+        ResetSelected(4);
+    }
+
+    /// <summary>
+    /// Resets all button visual to not be selected, then sets the button with
+    /// the corresponding importance value to be selected.
+    /// </summary>
+    /// <param name="importance">Importance value to select</param>
+    private void ResetSelected(int importance)
+    {
+        LowEnable = false;
+        MedEnable = false; 
+        HighEnable = false;
+        VHighEnable = false;
+
+        switch(importance)
+        {
+            case 1:
+                LowEnable = true;
+                break;
+            case 2:
+                MedEnable = true;
+                break;
+            case 3:
+                HighEnable = true;
+                break;
+            case 4:
+                VHighEnable = true;
+                break;
+        }
+
+        OnPropertyChanged(nameof(LowEnable));
+        OnPropertyChanged(nameof(MedEnable));
+        OnPropertyChanged(nameof(HighEnable));
+        OnPropertyChanged(nameof(VHighEnable));
     }
 }
