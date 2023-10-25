ee8c5021b490a461eb83f28ff0a82dfaf8d555c2
Rachel <83641601+Ithrendrial@users.noreply.github.com>
2023-10-25 13:05:29 +1300

    fix: prevent from opening unschedule dialogue box on scheduler background click.

--- TaskFlow/View/SchedulePage.xaml.cs
+++ TaskFlow/View/SchedulePage.xaml.cs
@@ -33,28 +33,32 @@ public partial class SchedulePage : ContentPage
     /// <param name="e"></param>
     public void OnSchedulerTapped(object sender, SchedulerTappedEventArgs e)
     {
-        SfPopup confirmPopup = new SfPopup() // Create pop up to confirm if user wants to unschedule the task
-        {
-            HeaderTitle = "Remove Task",
-            Message = "Do you want to unschedule this task?",
-            AutoSizeMode = PopupAutoSizeMode.Height,
-            AppearanceMode = PopupButtonAppearanceMode.TwoButton,
-            AcceptButtonText = "Remove",
-            DeclineButtonText = "Cancel",
-            AcceptCommand = new Command(() => ExecuteUnschedule(e)), // Method to unschedule, pass tapped appointment details as argument
-            ShowFooter = true,
-            HeightRequest = 180,
-            PopupStyle = new PopupStyle()
-            {
-                PopupBackground = Color.Parse("#341C4F"),
-                HeaderTextColor = Colors.White,
-                MessageTextColor = Colors.White,
-                AcceptButtonTextColor = Colors.White,
-                DeclineButtonTextColor = Colors.White
-            }
-        };
 
-        confirmPopup.Show();
+        if (e.Element == SchedulerElement.Appointment)
+        {
+            SfPopup confirmPopup = new SfPopup() // Create pop up to confirm if user wants to unschedule the task
+            {
+                HeaderTitle = "Remove Task",
+                Message = "Do you want to unschedule this task?",
+                AutoSizeMode = PopupAutoSizeMode.Height,
+                AppearanceMode = PopupButtonAppearanceMode.TwoButton,
+                AcceptButtonText = "Remove",
+                DeclineButtonText = "Cancel",
+                AcceptCommand = new Command(() => ExecuteUnschedule(e)), // Method to unschedule, pass tapped appointment details as argument
+                ShowFooter = true,
+                HeightRequest = 180,
+                PopupStyle = new PopupStyle()
+                {
+                    PopupBackground = Color.Parse("#341C4F"),
+                    HeaderTextColor = Colors.White,
+                    MessageTextColor = Colors.White,
+                    AcceptButtonTextColor = Colors.White,
+                    DeclineButtonTextColor = Colors.White
+                }
+            };
+
+            confirmPopup.Show();
+        }
     }
 
     /// <summary>
@@ -65,16 +69,13 @@ public partial class SchedulePage : ContentPage
     public void ExecuteUnschedule(SchedulerTappedEventArgs e)
     {
 
-        // If user tapped on an appointment, check if it was null and then send title and start time to RemoveTaskFromSchedule as identifiers
-        if (e.Element == SchedulerElement.SchedulerCell || e.Element == SchedulerElement.Appointment)
+        // Check if it was null and then send title and start time to RemoveTaskFromSchedule as identifiers
+        if (e.Appointments != null)
         {
-            if (e.Appointments != null)
-            {
-                String title = (e.Appointments[0] as SchedulerAppointment).Subject;
-                DateTime startTime = (e.Appointments[0] as SchedulerAppointment).StartTime;
+            String title = (e.Appointments[0] as SchedulerAppointment).Subject;
+            DateTime startTime = (e.Appointments[0] as SchedulerAppointment).StartTime;
 
-                ((SchedulerViewModel)BindingContext).RemoveTaskFromSchedule(title, startTime);
-            }
+            ((SchedulerViewModel)BindingContext).RemoveTaskFromSchedule(title, startTime);
         }
     }
 }
